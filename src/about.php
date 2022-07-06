fa2044a70054c24938ebe88c9972c215aa0d43e7
Muhammad Asharul Maali <masharul51@gmail.com>
2022-07-06 08:03:07 +0700

    Add files via upload

---
+++ src/about.php
@@ -0,0 +1,84 @@
+<!-- ====== About Section Start -->
+<section id="about" class="pt-20 lg:pt-[120px] pb-20 lg:pb-[120px] bg-[#f3f4fe]">
+    <div class="container">
+        <div class="bg-white wow fadeInUp" data-wow-delay=".2s">
+            <div class="flex flex-wrap -mx-4">
+                <div class="w-full px-4">
+                    <div class="
+                  lg:flex
+                  items-center
+                  justify-between
+                  border
+                  overflow-hidden
+                ">
+                        <div class="
+                    lg:max-w-[565px]
+                    xl:max-w-[640px]
+                    w-full
+                    py-12
+                    px-7
+                    sm:px-12
+                    md:p-16
+                    lg:py-9 lg:px-16
+                    xl:p-[70px]
+                  ">
+                            <span class="
+                      text-sm
+                      font-medium
+                      text-white
+                      py-2
+                      px-5
+                      bg-primary
+                      inline-block
+                      mb-5
+                    ">
+                                About Us
+                            </span>
+                            <h2 class="
+                      font-bold
+                      text-3xl
+                      sm:text-4xl
+                      2xl:text-[40px]
+                      sm:leading-snug
+                      text-dark
+                      mb-6
+                    ">
+                                Sejarah Toko Minyak Jaya hingga sekarang
+                            </h2>
+                            <p class="text-base text-body-color mb-9 leading-relaxed">
+                                Distributor produk-produk kebutuhan sehari-hari seperti Bimoli, Forvita, Harumas, Fitri, Sabrina . Terpercaya sejak tahun 1988.
+                            </p>
+                            <p class="text-base text-body-color mb-9 leading-relaxed">
+                                Berlokasi di kawasan lama perdagangan di Boyolali, Toko Minyak Racing (nama sebelum PT. Sakti Setia Santosa) berdagang hasil minyak bumi . Di tahun 1988 inilah, Toko Minyak Racing berubah menjadi Toko Minyak Jaya setelah ditunjuk sebagai distributor Minyak Terbesar di Boyolali.
+                            </p>
+                            <a href="javascript:void(0)" class="
+                      inline-flex
+                      items-center
+                      justify-center
+                      py-4
+                      px-6
+                      rounded
+                      text-white
+                      bg-primary
+                      text-base
+                      font-medium
+                      hover:bg-opacity-90 hover:shadow-lg
+                      transition
+                      duration-300
+                      ease-in-out
+                    ">
+                                Learn More
+                            </a>
+                        </div>
+                        <div class="text-center">
+                            <div class="relative inline-block z-10">
+                                <img src="assets/images/about/about-image.svg" alt="image" class="mx-auto lg:ml-auto" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ====== About Section End -->
---
+++ src/home.php
@@ -0,0 +1,18 @@
+<!-- Hero area start -->
+<?php include_once 'src/hero.php'; ?>
+<!-- Hero area end -->
+<!-- Features area start -->
+<?php include_once 'src/features.php'; ?>
+<!-- Features area end -->
+<!-- About area start -->
+<?php include_once 'src/about.php'; ?>
+<!-- About area end -->
+<!-- Team area start -->
+<?php include_once 'src/team.php'; ?>
+<!-- Team area end -->
+<!-- Faq Area Start -->
+<?php include_once 'src/faq.php'; ?>
+<!-- Faq Area End -->
+<!-- Testimonial Area Start -->
+<?php include_once 'src/testimonial.php'; ?>
+<!-- Testimonial Area End -->
